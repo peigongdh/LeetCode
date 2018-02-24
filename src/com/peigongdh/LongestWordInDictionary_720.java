a2e5590fa9b4ec8278ae96beb6a7cbd2b4b0994c
zhangpei <peigong.dh@qq.com>
2018-02-24 15:27:25 +0800

    update dfs solution

--- src/com/peigongdh/LongestWordInDictionary_720.java
+++ src/com/peigongdh/LongestWordInDictionary_720.java
@@ -1,9 +1,6 @@
 package com.peigongdh;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class LongestWordInDictionary_720 {
 
@@ -17,9 +14,9 @@ public class LongestWordInDictionary_720 {
 
     static class Solution {
 
-//        public String longestWord(String[] words) {
+//        String longestWord(String[] words) {
 //            Arrays.sort(words);
-//            Set<String> built = new HashSet<String>();
+//            Set<String> built = new HashSet<>();
 //            String res = "";
 //            for (String w : words) {
 //                if (w.length() == 1 || built.contains(w.substring(0, w.length() - 1))) {
@@ -30,25 +27,86 @@ public class LongestWordInDictionary_720 {
 //            return res;
 //        }
 
+//        String longestWord(String[] words) {
+//            String targetWord = "";
+//            Set<String> wordSet = new HashSet<>();
+//            Collections.addAll(wordSet, words);
+//            for (String word : words) {
+//                if (word.length() > targetWord.length() || word.length() == targetWord.length() && word.compareTo(targetWord) < 0) {
+//                    boolean isTargetWord = true;
+//                    for (int k = 1; k < word.length(); k ++) {
+//                        if (! wordSet.contains(word.substring(0, k))) {
+//                            isTargetWord = false;
+//                            break;
+//                        }
+//                    }
+//                    if (isTargetWord) {
+//                        targetWord = word;
+//                    }
+//                }
+//            }
+//            return targetWord;
+//        }
+
         String longestWord(String[] words) {
-            String targetWord = "";
-            Set<String> wordSet = new HashSet<>();
-            Collections.addAll(wordSet, words);
+            Trie trie = new Trie();
+            int index = 0;
             for (String word : words) {
-                if (word.length() > targetWord.length() || word.length() == targetWord.length() && word.compareTo(targetWord) < 0) {
-                    boolean isTargetWord = true;
-                    for (int k = 1; k < word.length(); k ++) {
-                        if (! wordSet.contains(word.substring(0, k))) {
-                            isTargetWord = false;
-                            break;
+                trie.insert(word, ++index); //indexed by 1
+            }
+            trie.words = words;
+            return trie.dfs();
+        }
+    }
+
+    static class Node {
+        char c;
+        Map<Character, Node> children = new HashMap();
+        int end;
+
+        Node(char c) {
+            this.c = c;
+        }
+    }
+
+    static class Trie {
+        Node root;
+        String[] words;
+
+        Trie() {
+            root = new Node('0');
+        }
+
+        public void insert(String word, int index) {
+            Node cur = root;
+            for (char c : word.toCharArray()) {
+                cur.children.putIfAbsent(c, new Node(c));
+                cur = cur.children.get(c);
+            }
+            cur.end = index;
+        }
+
+        // 按层遍历，只选取有数字的节点(node.end > 0)
+        public String dfs() {
+            String ans = "";
+            Stack<Node> stack = new Stack<>();
+            stack.push(root);
+            while (!stack.empty()) {
+                Node node = stack.pop();
+                if (node.end > 0 || node == root) {
+                    if (node != root) {
+                        String word = words[node.end - 1];
+                        if (word.length() > ans.length() ||
+                                word.length() == ans.length() && word.compareTo(ans) < 0) {
+                            ans = word;
                         }
                     }
-                    if (isTargetWord) {
-                        targetWord = word;
+                    for (Node nei : node.children.values()) {
+                        stack.push(nei);
                     }
                 }
             }
-            return targetWord;
+            return ans;
         }
     }
 }
