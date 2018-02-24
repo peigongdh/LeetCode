5b728ae725bd9733c1b1961b7a089d922ab7d727
zhangpei <peigong.dh@qq.com>
2018-02-24 10:53:19 +0800

    According to the solution I understand that word "yodn" can be built by "y", "yo", "yod", but "ewqz" can only built by "ew", "ewq", Lack of "e".

--- src/com/peigongdh/LongestWordInDictionary_720.java
+++ src/com/peigongdh/LongestWordInDictionary_720.java
@@ -1,6 +1,7 @@
 package com.peigongdh;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,40 +10,12 @@ public class LongestWordInDictionary_720 {
     public static void main(String[] args) {
         // String[] words = {"w", "wo", "wor", "worl", "world"};
         // String[] words = {"a", "banana", "app", "appl", "ap", "apply", "apple"};
-        String[] words = {"yo","ew","fc","zrc","yodn","fcm","qm","qmo","fcmz","z","ewq","yod","ewqz","y"};
+        String[] words = {"yo", "ew", "fc", "zrc", "yodn", "fcm", "qm", "qmo", "fcmz", "z", "ewq", "yod", "ewqz", "y"};
         String result = new Solution().longestWord(words);
         System.out.println(result);
     }
 
     static class Solution {
-        String longestWord(String[] words) {
-            Arrays.sort(words);
-            String targetWord = "";
-
-            for (int i = 0; i < words.length; i++) {
-                for (int j = 0; j < words.length; j++) {
-                    if (i == j) {
-                        continue;
-                    }
-                    if ("".equals(targetWord)) {
-                        targetWord = words[i];
-                        continue;
-                    }
-                    if (words[i].indexOf(words[j]) == 0) {
-                        // System.out.println(words[i] + "  " + words[j] + "  " + targetWord + "  " + words[i].compareTo(targetWord));
-                        if (words[i].length() > targetWord.length()) {
-                            targetWord = words[i];
-                        } else if (words[i].length() == targetWord.length()) {
-                            if (words[i].compareTo(targetWord) < 0) {
-                                targetWord = words[i];
-                            }
-                        }
-                    }
-                }
-            }
-
-            return targetWord;
-        }
 
 //        public String longestWord(String[] words) {
 //            Arrays.sort(words);
@@ -56,5 +29,26 @@ public class LongestWordInDictionary_720 {
 //            }
 //            return res;
 //        }
+
+        String longestWord(String[] words) {
+            String targetWord = "";
+            Set<String> wordSet = new HashSet<>();
+            Collections.addAll(wordSet, words);
+            for (String word : words) {
+                if (word.length() > targetWord.length() || word.length() == targetWord.length() && word.compareTo(targetWord) < 0) {
+                    boolean isTargetWord = true;
+                    for (int k = 1; k < word.length(); k ++) {
+                        if (! wordSet.contains(word.substring(0, k))) {
+                            isTargetWord = false;
+                            break;
+                        }
+                    }
+                    if (isTargetWord) {
+                        targetWord = word;
+                    }
+                }
+            }
+            return targetWord;
+        }
     }
 }
